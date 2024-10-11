ac1f923dfa63f9441dcf846801af385ee29ae670
oluwafemi <oo9701s@gre.ac.uk>
2024-10-11 15:34:58 +0100

    working with mongoose

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-require("dotenv").config();
 const path = require('path');
-
+require("dotenv").config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const mongoDbUrl = process.env.MONGO_DB_URL;
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -20,12 +20,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('670923c10c758a002a477c9e')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
+    User.findById('670923c10c758a002a477c9e')
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
@@ -33,6 +33,24 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+    .connect(
+        mongoDbUrl)
+    .then(_ => {
+        User.findOne().then(user => {
+            if (!user) {
+                const user = new User({
+                    name: 'Femi',
+                    email: 'developer.techbase@protonmail.com',
+                    cart: {
+                        items: []
+                    }
+                });
+                user.save();
+            }
+        });
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
