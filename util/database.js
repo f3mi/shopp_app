6abae3d8f3a3fb4d323f303c0d337cf83b7cd1d3
oluwafemi <oo9701s@gre.ac.uk>
2024-10-11 14:39:08 +0100

    working with NoSQL & using MongoDB

--- util/database.js
+++ util/database.js
@@ -1,8 +1,28 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const mongoDbUrl = process.env.MONGO_DB_URL;
 
-const sequelize = new Sequelize('node-complete', 'root', 'Adedamola@97', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = callback => {
+  MongoClient.connect(mongoDbUrl)
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
