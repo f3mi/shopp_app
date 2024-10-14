f3b36a771c5eb55f03615b36237ce474d4e6a0e7
oluwafemi <oo9701s@gre.ac.uk>
2024-10-14 17:31:10 +0100

    Adding Session and Cookies to app

--- controllers/admin.js
+++ controllers/admin.js
@@ -90,9 +90,9 @@ exports.getProducts = (req, res, next) => {
     .catch(err => console.log(err));
 };
 
-exports.postDeleteProduct =  (req, res, next) => {
+exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-   Product.findByIdAndDelete(prodId)
+  Product.findByIdAndRemove(prodId)
     .then(() => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
