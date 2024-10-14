bcfdc6fb09958b64a2cf84f6bff3fd1de45f5dd0
oluwafemi <oo9701s@gre.ac.uk>
2024-10-14 23:43:44 +0100

    added sessions and cookies

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ require("dotenv").config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const mongoDbUrl = process.env.MONGO_DB_URL;
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const MongoDbUrl = process.env.MONGO_DB_URL;
+
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const app = express();
+const store = MongoDBStore({
+    uri: MongoDbUrl,
+    collection: 'sessions',
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -19,15 +26,25 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'my secret', 
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+
+}))
 
 app.use((req, res, next) => {
-    User.findById('670923c10c758a002a477c9e')
-        .then(user => {
-            req.user = user;
-            next();
-        })
-        .catch(err => console.log(err));
-});
+    if (!req.session.user) {
+      return next();
+    }
+    User.findById(req.session.user._id)
+      .then(user => {
+        req.user = user;
+        next();
+      })
+      .catch(err => console.log(err));
+  });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -37,7 +54,7 @@ app.use(errorController.get404);
 
 mongoose
     .connect(
-        mongoDbUrl)
+        MongoDbUrl)
     .then(_ => {
         User.findOne().then(user => {
             if (!user) {
