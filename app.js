cc691f3d2ddef6cc91741db95aa48de93e0f98c7
oluwafemi <oo9701s@gre.ac.uk>
2024-10-15 01:03:06 +0100

    adding authentication

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
-const MongoDbUrl = process.env.MONGO_DB_URL;
+const csrf = require('csurf');
+const flash = require('connect-flash');
+const MONGODB_URI = process.env.MONGO_DB_URL;
 
 
 const errorController = require('./controllers/error');
@@ -13,9 +15,10 @@ const User = require('./models/user');
 
 const app = express();
 const store = MongoDBStore({
-    uri: MongoDbUrl,
+    uri: MONGODB_URI,
     collection: 'sessions',
 });
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -33,6 +36,8 @@ app.use(session({
     store: store,
 
 }))
+app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
     if (!req.session.user) {
@@ -46,6 +51,12 @@ app.use((req, res, next) => {
       .catch(err => console.log(err));
   });
 
+  app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
+  });
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
@@ -53,23 +64,11 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-    .connect(
-        MongoDbUrl)
-    .then(_ => {
-        User.findOne().then(user => {
-            if (!user) {
-                const user = new User({
-                    name: 'Femi',
-                    email: 'developer.techbase@protonmail.com',
-                    cart: {
-                        items: []
-                    }
-                });
-                user.save();
-            }
-        });
-        app.listen(3000);
-    })
-    .catch(err => {
-        console.log(err);
-    });
+  .connect(MONGODB_URI)
+  .then(result => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
