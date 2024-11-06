ede6e64eedf3bb92cdd8476b4b80dc769c5460e4
oluwafemi <oo9701s@gre.ac.uk>
2024-11-06 13:56:16 +0000

    manipulated DOM to delete products asynchronously

--- util/file.js
+++ util/file.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const deleteFile = (filePath) => {
     fs.unlink(filePath, (err) => {
-        if(err.errno == -4058){
+        if(err?.errno && err?.errno == -4058){
             return;
         }
         if (err) {
